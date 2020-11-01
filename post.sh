b529fc80e6d0a62cc67293b090925b297bdbe0de
redin0s <raffaele.bova00@gmail.com>
2020-11-01 15:02:04 +0100

    added poster script

---
+++ post.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+today=$(date +"%Y-%m-%d")
+postpath="./_posts/"
+
+echo "Enter the title of the post"
+read jekyllPost
+
+echo "Type the tags (separated by spaces, multi-word tags separated by a hyphen)"
+read postTags
+
+blogFileNameSpaces="$today-$jekyllPost"
+blogFileName=$(echo -e "$blogFileNameSpaces" | tr '[:upper:]' [:lower:] | tr '[:blank:]' - | tr -cd "[[:alnum:]-]")
+jekyllPostQuote=$(echo '"'$jekyllPost'"')
+postTagsCommas=$(echo -e $postTags | tr '[:blank:]' , )
+
+echo "---
+layout: post
+title:  $jekyllPostQuote
+date:   $today 09:00:00 +0200
+banner_image: "$bannerImageName"
+categories: "$postCategory"
+tags: ["$postTagsCommas"]
+---
+" >> $postpath/"$blogFileName".md
